3d541f2e8b19ae1acebeef04588fb5aecee337c7
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-08-30 15:33:56 +0200

    commit

--- src/main/java/brianpelinku/Application.java
+++ src/main/java/brianpelinku/Application.java
@@ -35,74 +35,58 @@ public class Application {
     public static void main(String[] args) {
         //EntityManager em = emf.createEntityManager();
 
-        DistributoreAutomatico distributore1 = new DistributoreAutomatico("Distributore1", "Location1", StatoDistributore.ATTIVO);
-        DistributoreAutomatico distributore2 = new DistributoreAutomatico("Distributore2", "Location2", StatoDistributore.FUORI_SERVIZIO);
-
-        RivenditoreAutorizzato rivenditore1 = new RivenditoreAutorizzato("Rivenditore1", "Location1", TipoRivenditore.EDICOLA);
-        RivenditoreAutorizzato rivenditore2 = new RivenditoreAutorizzato("Rivenditore2", "Location2", TipoRivenditore.TABACCAIO);
-
-        Utente utente1 = new Utente("Diego", "Basili");
-        Utente utente2 = new Utente("Gabriel", "Azamfiri");
-
-        Tessera tesseraUtente1 = new Tessera(utente1, LocalDate.of(2024, 5, 16));
-        Tessera tesseraUtente2 = new Tessera(utente2, LocalDate.of(2024, 2, 16));
-
-        Abbonamento abbonamento1 = new Abbonamento(LocalDate.of(2024, 8, 26), Durata.ANNUALE, tesseraUtente1, rivenditore1);
-        Abbonamento abbonamento2 = new Abbonamento(LocalDate.of(2024, 7, 20), Durata.MENSILE, tesseraUtente2, distributore1);
-        Abbonamento abbonamento3 = new Abbonamento(LocalDate.of(2024, 6, 12), Durata.SETTIMANALE, tesseraUtente2, distributore1);
-        Abbonamento abbonamento4 = new Abbonamento(LocalDate.of(2024, 2, 8), Durata.ANNUALE, tesseraUtente2, distributore1);
-
-        Biglietto biglietto1 = new Biglietto(LocalDate.of(2024, 6, 26), 90, false, tesseraUtente1, rivenditore2);
-        Biglietto biglietto2 = new Biglietto(LocalDate.of(2024, 7, 12), 90, false, tesseraUtente2, distributore1);
-
-        Tratta tratta1 = new Tratta("tratta1", "partenza1", "capolinea1", 60);
-        Tratta tratta2 = new Tratta("tratta2", "partenza2", "capolinea2", 60);
-
-
-        Mezzo mezzo1 = new Mezzo(TipoMezzo.TRAM, 100, StatoDelMezzo.SERVIZIO, trd.findById("3f2ce3aa-4117-4248-8d3a-cd68e6704f96"));
-        Mezzo mezzo2 = new Mezzo(TipoMezzo.AUTOBUS, 40, StatoDelMezzo.SERVIZIO, trd.findById("4f702139-88da-44b7-9921-1c32c58ed534"));
-
-        // StatoMezzo statoMezzo1 = new StatoMezzo(LocalDate.now(), StatoDelMezzo.SERVIZIO, md.findById("2279ba24-b8a6-48c3-b1b4-4c06fc52084a"));
-
-
-        GiroTratta giro1tratta1 = new GiroTratta(mezzo1, tratta1, LocalDateTime.of(2024, 7, 12, 10, 30), LocalDateTime.of(2024, 7, 12, 11, 20));
-        GiroTratta giro2tratta1 = new GiroTratta(md.findById("0db1a909-1630-4c0c-993d-84e8f489afba"), trd.findById("3f2ce3aa-4117-4248-8d3a-cd68e6704f96"), LocalDateTime.of(2024, 8, 22, 12, 0), LocalDateTime.of(2024, 8, 22, 12, 58));
-        GiroTratta giro3tratta1 = new GiroTratta(mezzo2, tratta1, LocalDateTime.of(2024, 8, 22, 12, 58), LocalDateTime.of(2024, 8, 22, 14, 1));
-        GiroTratta giro4tratta1 = new GiroTratta(mezzo2, tratta1, LocalDateTime.of(2024, 8, 22, 14, 2), LocalDateTime.of(2024, 8, 22, 15, 5));
-
-
-        Timbratura timbratura1 = new Timbratura(LocalDateTime.of(2024, 7, 12, 10, 31), mezzo1, biglietto1);
-        Timbratura timbratura2 = new Timbratura(LocalDateTime.of(2024, 8, 22, 12, 10), mezzo2, biglietto2);
-
-        //ped.updateStatoDistributore("88a2b9f8-e79f-406d-9b26-afe52b246569", StatoDistributore.FUORI_SERVIZIO);
-
+//        DistributoreAutomatico distributore1 = new DistributoreAutomatico("Distributore1", "Location1", StatoDistributore.ATTIVO);
+//        DistributoreAutomatico distributore2 = new DistributoreAutomatico("Distributore2", "Location2", StatoDistributore.FUORI_SERVIZIO);
+//        RivenditoreAutorizzato rivenditore1 = new RivenditoreAutorizzato("Rivenditore1", "Location1", TipoRivenditore.EDICOLA);
+//        RivenditoreAutorizzato rivenditore2 = new RivenditoreAutorizzato("Rivenditore2", "Location2", TipoRivenditore.TABACCAIO);
+//        Utente utente1 = new Utente("Diego", "Basili");
+//        Utente utente2 = new Utente("Gabriel", "Azamfiri");
+//        Tessera tesseraUtente1 = new Tessera(utente1, LocalDate.of(2024, 5, 16));
+//        Tessera tesseraUtente2 = new Tessera(utente2, LocalDate.of(2024, 2, 16));
+//        Abbonamento abbonamento1 = new Abbonamento(LocalDate.of(2024, 8, 26), Durata.ANNUALE, tesseraUtente1, rivenditore1);
+//        Abbonamento abbonamento2 = new Abbonamento(LocalDate.of(2024, 7, 20), Durata.MENSILE, tesseraUtente2, distributore1);
+//        Abbonamento abbonamento3 = new Abbonamento(LocalDate.of(2024, 6, 12), Durata.SETTIMANALE, tesseraUtente2, distributore1);
+//        Abbonamento abbonamento4 = new Abbonamento(LocalDate.of(2024, 2, 8), Durata.ANNUALE, tesseraUtente2, distributore1);
+//        Biglietto biglietto1 = new Biglietto(LocalDate.of(2024, 6, 26), 90, false, tesseraUtente1, rivenditore2);
+//        Biglietto biglietto2 = new Biglietto(LocalDate.of(2024, 7, 12), 90, false, tesseraUtente2, distributore1);
+//        Tratta tratta1 = new Tratta("tratta1", "partenza1", "capolinea1", 60);
+//        Tratta tratta2 = new Tratta("tratta2", "partenza2", "capolinea2", 60);
+//        Mezzo mezzo1 = new Mezzo(TipoMezzo.TRAM, 100, StatoDelMezzo.SERVIZIO, trd.findById("3f2ce3aa-4117-4248-8d3a-cd68e6704f96"));
+//        Mezzo mezzo2 = new Mezzo(TipoMezzo.AUTOBUS, 40, StatoDelMezzo.SERVIZIO, trd.findById("4f702139-88da-44b7-9921-1c32c58ed534"));
+//        // StatoMezzo statoMezzo1 = new StatoMezzo(LocalDate.now(), StatoDelMezzo.SERVIZIO, md.findById("2279ba24-b8a6-48c3-b1b4-4c06fc52084a"));GiroTratta giro1tratta1 = new GiroTratta(mezzo1, tratta1, LocalDateTime.of(2024, 7, 12, 10, 30), LocalDateTime.of(2024, 7, 12, 11, 20));
+//        GiroTratta giro2tratta1 = new GiroTratta(md.findById("0db1a909-1630-4c0c-993d-84e8f489afba"), trd.findById("3f2ce3aa-4117-4248-8d3a-cd68e6704f96"), LocalDateTime.of(2024, 8, 22, 12, 0), LocalDateTime.of(2024, 8, 22, 12, 58));
+//        GiroTratta giro3tratta1 = new GiroTratta(mezzo2, tratta1, LocalDateTime.of(2024, 8, 22, 12, 58), LocalDateTime.of(2024, 8, 22, 14, 1));
+//        GiroTratta giro4tratta1 = new GiroTratta(mezzo2, tratta1, LocalDateTime.of(2024, 8, 22, 14, 2), LocalDateTime.of(2024, 8, 22, 15, 5));
+//        Timbratura timbratura1 = new Timbratura(LocalDateTime.of(2024, 7, 12, 10, 31), mezzo1, biglietto1);
+//        Timbratura timbratura2 = new Timbratura(LocalDateTime.of(2024, 8, 22, 12, 10), mezzo2, biglietto2);
+//        //ped.updateStatoDistributore("88a2b9f8-e79f-406d-9b26-afe52b246569", StatoDistributore.FUORI_SERVIZIO);
         /*ped.save(distributore1);
         ped.save(distributore2);
         ped.save(rivenditore1);
-        ped.save(rivenditore2);*/
-//        ud.save(utente1);
-//        ud.save(utente2);
-//        td.save(tesseraUtente1);
-//        td.save(tesseraUtente2);
-//        ad.save(abbonamento3);
-//        ad.save(abbonamento4);
-//        ad.save(abbonamento1);
-//        ad.save(abbonamento2);
-//        bd.save(biglietto1);
-//        bd.save(biglietto2);
-//        trd.save(tratta1);
-//        trd.save(tratta2);
-        //  smd.save(statoMezzo1);
-//        smd.save(statoMezzo2);
-        //md.save(mezzo1);
-        //md.save(mezzo2);
-//        smd.save(statoMezzo2);
-//        gd.save(giro1tratta1);
-        //gd.save(giro2tratta1);
-//        gd.save(giro3tratta1);
-//        gd.save(giro4tratta1);
-//        timbd.save(timbratura1);
-//        timbd.save(timbratura2);
+        ped.save(rivenditore2);
+        ud.save(utente1);
+        ud.save(utente2);
+        td.save(tesseraUtente1);
+        td.save(tesseraUtente2);
+        ad.save(abbonamento3);
+        ad.save(abbonamento4);
+        ad.save(abbonamento1);
+        ad.save(abbonamento2);
+        bd.save(biglietto1);
+        bd.save(biglietto2);
+        trd.save(tratta1);
+        trd.save(tratta2);
+        smd.save(statoMezzo1);
+        smd.save(statoMezzo2);
+        md.save(mezzo1);
+        md.save(mezzo2);
+        smd.save(statoMezzo2);
+        gd.save(giro1tratta1);
+        gd.save(giro2tratta1);
+        gd.save(giro3tratta1);
+        gd.save(giro4tratta1);
+        timbd.save(timbratura1);
+        timbd.save(timbratura2);*/
 
 
         boolean start = true;
